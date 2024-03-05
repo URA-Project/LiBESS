7c3990e7db8869077e8fcce6858194d16dbd35d9
Vu Vinh <vuxuanvinh12@gmail.com>
2024-03-05 13:08:39 +0700

    chinh sua path trong all_packages.py

--- src/all_packages.py
+++ src/all_packages.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import pandas as pd
 
+#Day la absolute path cua may em, neu can kiem tra thu co the su dung relative path hay khong
 PATHS = {
     "data": "/Users/vinhvu/LiBESS/data/data_test.csv",
     "resource": "/Users/vinhvu/LiBESS/data/resource.csv",
